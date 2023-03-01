82020e9093b16cab37e9f6b81db25ed23a8718b2
Husniddin <NusurmatovPro@gmail.com>
2023-03-01 12:58:14 +0500

    start competitive programming!

---
+++ SoftUni/CSharpFun/BasicSyntaxExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// Problem 6: Strong Number
+Console.Clear();
+
+int Factorial(int n)
+{
+    int f = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        f *= i;   
+    }
+
+    return f;
+}
+
+string num = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < num.Length; i++)
+{
+    sum += Factorial(num[i] - 48);
+}
+
+Console.Write(int.Parse(num) != sum ? "no" : "yes");
