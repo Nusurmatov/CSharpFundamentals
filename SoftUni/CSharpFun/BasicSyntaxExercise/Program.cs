2b3bc0d2165101dff7e2d1cb9886c9ab03f8120a
Nusurmatov Husniddin <NusurmatovPro@gmail.com>
2023-03-02 08:38:07 +0500

    finish BasicSyntaxExercise

--- SoftUni/CSharpFun/BasicSyntaxExercise/Program.cs
+++ SoftUni/CSharpFun/BasicSyntaxExercise/Program.cs
@@ -1,13 +1,23 @@
-﻿// Problem 9: Padawan Equipment
+﻿// Problem 11: *Orders
 Console.Clear();
 
 int n = int.Parse(Console.ReadLine());
+decimal sum = 0.0m;
+decimal[] sumsPC = new decimal[n];
 
-for (int i = 1; i <= n; i++)
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j < i; j++)
-    {
-        Console.Write($"{i} ");
-    }
-    Console.WriteLine();
+    decimal pPC = decimal.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int cCount = int.Parse(Console.ReadLine());
+
+    sum += pPC * days * cCount;
+    sumsPC[i] = pPC * days * cCount;
 }
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("The price for the coffee is: ${0:0.00}", sumsPC[i]);
+}
+
+Console.Write("Total: ${0:0.00}", sum);
