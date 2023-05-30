c77d60cde82634c61e65b590ab6587a9de5dc79f
Nusurmatov <NusurmatovPro@gmail.com>
2023-05-30 07:01:08 +0500

    finish exercise 7 of DataTypesVariablesExercise

---
+++ CSharpFundamentals/ArraysLab/Program.cs
@@ -0,0 +1,21 @@
+﻿// Problem 8: Condense Array to Number:
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+if (numbers.Length == 1)
+{
+    Console.Write(numbers.First());    
+}
+else
+{
+    int[] condensed = new int[numbers.Length]; 
+
+    for (int i = 0; i < numbers.Length - 2; i++)
+    {
+        for (int j = 0; j < numbers.Length - i - 1; j++)
+        {
+            condensed[j] = numbers[j] + numbers[j+1];
+        }
+        Array.Copy(condensed, numbers, condensed.Length);
+    }
+
+    Console.Write(numbers[0] + numbers[1]);
+}
---
+++ CSharpFundamentals/DataTypesVariablesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿// Problem 7 - Water Overflow:
+
+short n = Convert.ToInt16(Console.ReadLine());
+short currentQuantity = 0;
+for (short i = 0; i < n; i++)
+{
+    short liters = Convert.ToInt16(Console.ReadLine());
+    if (liters > 255)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+    else
+    {
+        currentQuantity += liters;
+    }
+}
+
+Console.Write(currentQuantity);
