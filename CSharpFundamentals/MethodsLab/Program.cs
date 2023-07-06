30af2d665ccc45cea8e9296303b7790329647b64
Nusurmatov <NusurmatovPro@gmail.com>
2023-07-06 16:48:04 +0500

    finish Methods Lab, Exercise and More

---
+++ CSharpFundamentals/MethodsLab/Program.cs
@@ -0,0 +1,14 @@
+﻿// Problem 11 - Math Operations:
+int a = Convert.ToInt32(Console.ReadLine());
+var @operator = Console.ReadLine();
+int b = Convert.ToInt32(Console.ReadLine());
+double result = Calculate(a, @operator, b);
+Console.Write(result);
+
+double Calculate(int a, string? @operator, int b) => @operator switch
+{
+    "+" => a + b,
+    "-" => a - b,
+    "*" => a * b,
+    _ => a / b
+};
